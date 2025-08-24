3fe73cd1cd0aaab0d50f9ee5f41d3ccc270c8143
Kevin-Rengifo0124 <krengifo0124@gmail.com>
2025-08-24 02:57:57 -0500

    feat: creación de componentes de admin, users y configuración de Ng Zorro

---
+++ autos-web/src/app/modules/users/components/users-dashboard/users-dashboard.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-users-dashboard',
+  imports: [],
+  templateUrl: './users-dashboard.html',
+  styleUrl: './users-dashboard.scss'
+})
+export class UsersDashboard {
+
+}
