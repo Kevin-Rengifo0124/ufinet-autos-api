63f0cf201eab0a5c0c3f286b34aa2085fb82bbf3
Kevin-Rengifo0124 <krengifo0124@gmail.com>
2025-08-24 02:04:24 -0500

    Primer commit, se hizo la configuracion de las dependencias de spring boot y las conexiones a la base de datos

---
+++ autos-api/src/test/java/com/ufinet/autos/autos_api/AutosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufinet.autos.autos_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
